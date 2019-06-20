a35263b866f7923ff9307402dd2baad0db629812
Daniil Novikov <dannovik30@gmail.com>
2019-06-20 16:11:34 +0300

    add build folder for deployment

---
+++ build/precache-manifest.4a9e60a5cd5beb3942f68c368772f31b.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "86dd4197bc664e69cc2dcba53bd2e6bc",
+    "url": "/static/media/opensanssemibold.86dd4197.woff2"
+  },
+  {
+    "revision": "56a7974a4047d73190b8",
+    "url": "/static/css/main.45b78897.chunk.css"
+  },
+  {
+    "revision": "74823f41948b9f519f8a",
+    "url": "/static/js/1.74823f41.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "aac07fc6db99e7eb977e36c5b2571716",
+    "url": "/static/media/opensanslight.aac07fc6.woff2"
+  },
+  {
+    "revision": "313824e85557a721d326424b6eb63fec",
+    "url": "/static/media/opensansbold.313824e8.woff2"
+  },
+  {
+    "revision": "811c8baeca3bb3329488f729979c1fbc",
+    "url": "/static/media/opensans.811c8bae.woff"
+  },
+  {
+    "revision": "48aa7ad98beedc63d82925f45fd8e72c",
+    "url": "/static/media/opensans.48aa7ad9.woff2"
+  },
+  {
+    "revision": "9a0cfeda5ce5de1674106cd0a66ed55e",
+    "url": "/static/media/opensanslight.9a0cfeda.woff"
+  },
+  {
+    "revision": "fbf7f082bb8abfeef11c89c77452f796",
+    "url": "/static/media/opensansitalic.fbf7f082.woff2"
+  },
+  {
+    "revision": "56a7974a4047d73190b8",
+    "url": "/static/js/main.56a7974a.chunk.js"
+  },
+  {
+    "revision": "299b3be898e809232985246037e615b9",
+    "url": "/static/media/opensanssemibold.299b3be8.woff"
+  },
+  {
+    "revision": "6f05808b4f6a11d3feb1716291c7c038",
+    "url": "/static/media/opensansbold.6f05808b.woff"
+  },
+  {
+    "revision": "576f55c34a35e2d1e8d46dc916dd53de",
+    "url": "/static/media/opensansitalic.576f55c3.woff"
+  },
+  {
+    "revision": "d2a705b59e370eaa5d145962f3b1ade1",
+    "url": "/static/media/opensans.d2a705b5.ttf"
+  },
+  {
+    "revision": "483351b66d7f49ef56b83ecb00dd3712",
+    "url": "/static/media/opensanssemibold.483351b6.ttf"
+  },
+  {
+    "revision": "25ad8d1b471db8cc26d864db84a86bd5",
+    "url": "/static/media/opensanslight.25ad8d1b.ttf"
+  },
+  {
+    "revision": "b87efff09ad1fd0e8da7f17ce94c4a83",
+    "url": "/static/media/opensansbold.b87efff0.ttf"
+  },
+  {
+    "revision": "bf2f6b57cd6438b1e1ca22dd53c319c0",
+    "url": "/static/media/opensansitalic.bf2f6b57.ttf"
+  },
+  {
+    "revision": "883e8c589fa0fd7482bf1e2259fe7fa1",
+    "url": "/index.html"
+  }
+];
